931b49f3767329d72129a4eba966d3ffcbaccbeb
Manuel Penz <m.penz@student.uibk.ac.at>
2021-12-20 17:11:57 +0100

    update graph

--- graph.py
+++ graph.py
@@ -1,53 +1,70 @@
 from collections import defaultdict
 
 class Graph():
+    """
+    this class represents a graph read from the graph text-file
+
+    contains a dictionary of lists of successors
+                a dictionary of lists of predecessors
+                a dictionary of tuples of nodes (nodeID, nodeType)
+    """
 
     def __init__(self):
         self.successors = defaultdict(list)
         self.predecessors = defaultdict(list)
         self.nodes = defaultdict(tuple)
 
-    def getNodes(self):
-        return self.nodes.keys()
-
-    def getNodesDescriptors(self):
-        return self.nodes
-
-    def getNodePlayerID(self, node):
-        return self.nodes[node]
-
     def addNode(self, node, info):
+        """
+        adds a node the graph with it's type
+        :param node: nodeID
+        :param info: nodeTyoe
+        """
         self.nodes[node] = info
 
-    def removeNode(self, node):
-        del self.nodes[node]
+    def getNodes(self):
+        """
+        returns all node's ID
+        :return: node IDs
+        """
+        return self.nodes.keys()
 
-    def getSuccessors(self, node):
-        return self.successors[node]
+    def getNodePlayerID(self, node):
+        """
+        returns one node's ID
+        :param node: returns one node's ID
+        :return: ID of node
+        """
+        return self.nodes[node]
 
     def addSuccessor(self, node, successor):
+        """
+        adds a successor to node
+        :param node: node where to add a successor
+        :param successor: successor node
+        """
         self.successors[node].append(successor)
 
-    def delSuccessor(self, node, successor):
-        self.successors[node].remove(successor)
-
-    def getPredecessors(self, node):
-        return self.predecessors[node]
+    def getSuccessors(self, node):
+        """
+        returns all node's successors
+        :param node: the given node
+        :return: list of node's successors
+        """
+        return self.successors[node]
 
     def addPredecessor(self, node, predecessor):
-        #print("PRED")
+        """
+        adds a predecessor to node
+        :param node: node where to add a predecessor
+        :param predecessor: predecessor node
+        """
         self.predecessors[node].append(predecessor)
 
-    def delPredecessor(self, node, predecessor):
-        self.predecessors[node].remove(predecessor)
-
-    def __str__(self):
-        txt = ""
-
-        for node in self.nodes:
-            txt += str(node) + " " + str(self.nodes[node]) + "\n" + str(node) + " -> "
-            for succ in self.successors[node]:
-                txt += str(succ) + ", "
-            txt += "\n"
-
-        return txt
+    def getPredecessors(self, node):
+        """
+        returns all node's predecessors
+        :param node: the given node
+        :return: list of node's predecessors
+        """
+        return self.predecessors[node]
